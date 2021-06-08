ee135358303a2398b66aa285dbb8b51f60fa852c
Le-TRex <clarembo@gmail.com>
2021-06-08 15:10:34 +0200

    :card_file_box: add demanded by client attibutes to User

--- src/main/java/com/example/battledevback/models/User.java
+++ src/main/java/com/example/battledevback/models/User.java
@@ -1,6 +1,8 @@
 package com.example.battledevback.models;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
 public class User {
@@ -9,13 +11,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String name;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private LocalDate dateOfBirth;
 
     /* CONSTRUCTORS */
     public User(){}
 
-    public User(String name) {
-        this.name = name;
+    public User(String firstName, String lastName, String email, String gender, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
     }
 
     /* GETTERS AND SETTERS */
@@ -28,11 +38,43 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
     }
 }
